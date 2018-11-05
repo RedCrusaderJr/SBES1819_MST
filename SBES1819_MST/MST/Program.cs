4d15cd71b5467c67e852744088c9c9914f97c6e7
dunjabursac <39524596+dunjabursac@users.noreply.github.com>
2018-11-05 16:03:47 +0100

    Dunja: MST
    ispis svih pokrenutih procesa
    (processName, processId, user)

--- SBES1819_MST/MST/Program.cs
+++ SBES1819_MST/MST/Program.cs
@@ -1,15 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Management;
 
 namespace MST
 {
     class Program
     {
+        static string GetProcessOwner(int processId)
+        {
+            string query = "Select * From Win32_Process Where ProcessID = " + processId;
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
+            ManagementObjectCollection processList = searcher.Get();
+
+            foreach (ManagementObject obj in processList)
+            {
+                string[] argList = new string[] { string.Empty, string.Empty };
+                int returnVal = Convert.ToInt32(obj.InvokeMethod("GetOwner", argList));
+                if (returnVal == 0)
+                {
+                    // return DOMAIN\user
+                    return argList[1] + "\\" + argList[0];
+                }
+            }
+
+            return "NO OWNER";
+        }
+
         static void Main(string[] args)
         {
+            Process[] processlist = Process.GetProcesses(Environment.MachineName);
+
+            foreach (Process theprocess in processlist)
+            {
+                if(theprocess.ProcessName == "notepad")
+                Console.WriteLine("Process: {0} ID: {1}, process user: {2}", theprocess.ProcessName, theprocess.Id, GetProcessOwner(theprocess.Id));
+            }
+
+            Console.ReadLine();
         }
     }
 }
