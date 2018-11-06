6572823dc948cae0eee22253dbd210fe9cb56a39
JovanDavidovic <jovanvisoki96@gmail.com>
2018-11-06 16:24:51 +0100

    xml

--- SBES1819_MST/MST/Program.cs
+++ SBES1819_MST/MST/Program.cs
@@ -18,6 +18,26 @@ namespace MST
 
             //TODO open host XML...
 
+            //**************************************************************
+
+            List<XML_Node> lista = new List<XML_Node>();
+
+            lista.Add(new XML_Node("aaa", "aaa", "aaa"));
+            lista.Add(new XML_Node("bbb", "bbb", "bbb"));
+            lista.Add(new XML_Node("ccc", "ccc", "ccc"));
+
+            XML_Worker.Instance().XML_Write(lista);             // Poziv upisa
+                
+            List<XML_Node> lista2 = new List<XML_Node>();       // xml se nalazi u debag folderu
+            lista2 = XML_Worker.Instance().XML_Read();          // Poziv iscitavanja
+
+            foreach (XML_Node n in lista2)
+            {
+                Console.WriteLine(n.UserId + " " + n.UserGroup + " " + n.ProcessName);
+            }
+
+            //**************************************************************
+
             ThreadFunction tf = new ThreadFunction();
 
             Thread t = new Thread(tf.DetectProcesses);
