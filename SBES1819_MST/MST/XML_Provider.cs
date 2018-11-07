f4ee4f4cce2cdb19247efeaaf25ce62e7d2598ba
dunjabursac <39524596+dunjabursac@users.noreply.github.com>
2018-11-07 14:33:00 +0100

    Dunja: XML_Provider
    Realizacija 8 metoda koje MST izlaze XML_Client-u

--- SBES1819_MST/MST/XML_Provider.cs
+++ SBES1819_MST/MST/XML_Provider.cs
@@ -14,44 +14,104 @@ namespace MST
         // one rade sa upisom i citanjem liste<XLM_node> u/iz fajla
         // Jovan i Dunja po 4 komada
 
+
+        private List<XML_Node> black_list = new List<XML_Node>();   // TODO: Dunja - da li ovo treba u konstruktoru klase ? 
+
+        public XML_Provider()
+        {
+            black_list = XML_Worker.Instance().XML_Read();
+        }
+
         public void AllowProcess(string processName)
         {
-            throw new NotImplementedException();
+            black_list = XML_Worker.Instance().XML_Read();
+
+            black_list.RemoveAll(n => (n.ProcessName == processName));
+
+            //foreach(XML_Node n in black_list)
+            //{
+            //    if(n.ProcessName == processName)
+            //    {
+            //        black_list.Remove(n);
+            //    }
+            //}
+
+            XML_Worker.Instance().XML_Write(black_list);
         }
 
         public void BanGroup(string groupID, string processName)
         {
-            throw new NotImplementedException();
+            black_list = XML_Worker.Instance().XML_Read();
+
+            XML_Node n = new XML_Node("*", groupID, processName);
+            black_list.Add(n);
+
+            XML_Worker.Instance().XML_Write(black_list);
         }
 
         public void BanUser(string userID, string processName)
         {
-            throw new NotImplementedException();
+            black_list = XML_Worker.Instance().XML_Read();
+
+            XML_Node n = new XML_Node(userID, "*", processName);
+            black_list.Add(n);
+
+            XML_Worker.Instance().XML_Write(black_list);
         }
 
         public void BanUserInGroup(string userID, string groupID, string processName)
         {
-            throw new NotImplementedException();
+            black_list = XML_Worker.Instance().XML_Read();
+
+            XML_Node n = new XML_Node(userID, groupID, processName);
+            black_list.Add(n);
+
+            XML_Worker.Instance().XML_Write(black_list);
         }
 
         public void ForbidProcess(string processName)
         {
-            throw new NotImplementedException();
+            black_list = XML_Worker.Instance().XML_Read();
+
+            XML_Node n = new XML_Node("*", "*", processName);
+            black_list.Add(n);
+
+            XML_Worker.Instance().XML_Write(black_list);
         }
 
         public void LiftGroupBan(string groupID, string processName)
         {
-            throw new NotImplementedException();
+            black_list = XML_Worker.Instance().XML_Read();
+
+            black_list.RemoveAll(n => ((n.UserGroup == groupID) && (n.ProcessName == processName)));
+
+            //foreach(XML_Node n in black_list)       // TODO: da li moze ovakvo brisanje u listi
+            //{
+            //    if((n.UserGroup == groupID) && (n.ProcessName == processName))
+            //    {
+            //        black_list.Remove(n);
+            //    }
+            //}
+
+            XML_Worker.Instance().XML_Write(black_list);
         }
 
         public void LiftUserBan(string userID, string processName)
         {
-            throw new NotImplementedException();
+            black_list = XML_Worker.Instance().XML_Read();
+
+            black_list.RemoveAll(n => ((n.UserId == userID) && (n.ProcessName == processName)));
+
+            XML_Worker.Instance().XML_Write(black_list);
         }
 
         public void LiftUserInGroupBan(string userID, string groupID, string processName)
         {
-            throw new NotImplementedException();
+            black_list = XML_Worker.Instance().XML_Read();
+
+            black_list.RemoveAll(n => ((n.UserId == userID) && (n.UserGroup == groupID) && (n.ProcessName == processName)));
+
+            XML_Worker.Instance().XML_Write(black_list);
         }
     }
 }
